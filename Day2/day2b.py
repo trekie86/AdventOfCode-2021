da13084edb8910ff04678a36ad2f545ec88663b0
jonathanransom <jransom@chickasawphone.net>
2021-12-02 09:11:56 -0600

    Day 2 Code completed.

---
+++ Day2/day2b.py
@@ -0,0 +1,33 @@
+with open("data.txt") as file:
+    lines = file.readlines()
+
+horizontal = 0
+depth = 0
+aim = 0
+forward = "forward"
+down = "down"
+up = "up"
+
+for line in lines:
+    if forward in line:
+        for char in line.split():
+            if char.isdigit():
+                num = (int(char))
+        horizontal += num
+        add_depth = aim * num
+        depth += add_depth
+
+    if down in line:
+        for char in line.split():
+            if char.isdigit():
+                num = (int(char))
+        aim += num
+
+    if up in line:
+        for char in line.split():
+            if char.isdigit():
+                num = (int(char))
+        aim -= num
+
+total = horizontal * depth
+print(total)
