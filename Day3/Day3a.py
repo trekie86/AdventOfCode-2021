5aea09f47ba44a44eea897351ee54d8529df0189
jonathanransom <jransom@chickasawphone.net>
2021-12-06 09:19:48 -0600

    Updated formatting

--- Day3/Day3a.py
+++ Day3/Day3a.py
@@ -1,9 +1,9 @@
-def most_frequent(data_list):
-    return max(set(data_list), key=data_list.count)
+def most_frequent(List):
+    return max(set(List), key = List.count)
 
 
-def least_frequent(data_list):
-    return min(set(data_list), key=data_list.count)
+def least_frequent(List):
+    return min(set(List), key = List.count)
 
 
 with open("data.txt") as file:
@@ -12,17 +12,18 @@ with open("data.txt") as file:
 gamma = []
 epsilon = []
 digits = []
-
 i = 0
-while i < 5:
+
+while i < 12:
     for line in lines:
+        line[i]
         digits.append(line[i])
     gamma.append(most_frequent(digits))
     digits.clear()
     i += 1
 
 i = 0
-while i < 5:
+while i < 12:
     for line in lines:
         line[i]
         digits.append(line[i])
@@ -35,8 +36,6 @@ gamma_rate_int = int(gamma_rate_bi, 2)
 epsilon_rate_bi = ("".join([str(i) for i in epsilon]))
 epsilon_rate_int = int(epsilon_rate_bi, 2)
 
-power = gamma_rate_int * epsilon_rate_int
+total = gamma_rate_int * epsilon_rate_int
 
-print(gamma_rate_bi)
-print(epsilon_rate_bi)
-print(power)
+print(total)
--- Day3/Day3b.py
+++ Day3/Day3b.py
@@ -1,74 +1,98 @@
+# Most Frequent Digit
 def most_frequent(data_list):
-    print("---Get Most Frequent---")
-    print("data_list:" + str(data_list))
+    # print("data_list:" + str(data_list))
     zeros = data_list.count(0)
     ones = data_list.count(1)
-    print("zeros:" + str(zeros))
-    print("ones:" + str(ones))
+    # print("zeros:" + str(zeros))
+    # print("ones:" + str(ones))
     if zeros > ones:
-        print("return 0")
         return 0
     if ones >= zeros:
-        print("return 1")
         return 1
 
 
+# Least Frequent Digit
 def least_frequent(data_list):
-    return min(set(data_list), key=data_list.count)
+    # print("data_list:" + str(data_list))
+    zeros = data_list.count(0)
+    ones = data_list.count(1)
+    # print("zeros:" + str(zeros))
+    # print("ones:" + str(ones))
+    if zeros <= ones:
+        return 0
+    if ones < zeros:
+        return 1
 
 
+# Open Data File
 with open("data.txt") as file:
     lines = file.read().splitlines()
+    # print("Lines:" + str(lines))
 
+index = 12
 oxygen = []
 co2 = []
 digits = []
-contenders = []
-index = 5
 
-# Find Most Frequent Digit
-# Put Items with Most Frequent Digit in List
+# Get Oxygen
 contenders = lines
-print("contenders:" + str(contenders))
 i = 0
 while i < index:
+    # Get Fist Digits of Contenders
     for contender in contenders:
         digits.append(int(contender[i]))
-    oxygen.append(most_frequent(digits))
+    # print("Digits:" + str(digits))
+
+    # Get Most Frequent Digit
+    max_num = (most_frequent(digits))
+    # print("Max_Num:" + str(max_num))
+
+    # Clear List
     digits.clear()
-    print("i:" + str(i))
-    print("Oxygen:" + str(oxygen))
-    result = list(filter(lambda x: (x[i] == oxygen[i]), contenders))
-    contenders = result
-    print("Contenders:" + str(contenders))
+
+    # print("Contenders:" + str(contenders))
+
+    # Match Starting Digit to Contenders
+    results = list(filter(lambda x: (x[i] == str(max_num)), contenders))
+    # print("Results:" + str(results))
+    contenders = results
     i += 1
 
-# Find Least Common Digit
-# Put Items with Least Frequent Digit in List
-contenders = lines
-print("contenders:" + str(contenders))
-i = 0
-while i < index:
-    for contender in contenders:
-        digits.append(contender[i])
-    co2.append(least_frequent(digits))
-    digits.clear()
-    print("i:" + str(i))
-    print("co2[]:" + co2[i])
-    result = list(filter(lambda x: (x[i] == co2[i]), contenders))
-    contenders = result
-    print("Contenders:" + str(contenders))
-    i += 1
-
-oxygen_bi = ("".join([str(i) for i in oxygen]))
+oxygen_bi = ("".join([str(i) for i in results]))
 oxygen_int = int(oxygen_bi, 2)
-co2_bi = ("".join([str(i) for i in co2]))
-co2_int = int(co2_bi, 2)
-
-life_support = oxygen_int * co2_int
-
 print("Oxygen_Binary:" + str(oxygen_bi))
 print("Oxygen_Int:" + str(oxygen_int))
+
+# Get CO2
+contenders = lines
+i = 0
+while i < index:
+    # Get Fist Digits of Contenders
+    for contender in contenders:
+        digits.append(int(contender[i]))
+    # print("Digits:" + str(digits))
+
+    # Get Most Frequent Digit
+    min_num = (least_frequent(digits))
+    # print("Max_Num:" + str(min_num))
+
+    # Clear List
+    digits.clear()
+
+    # print("Contenders:" + str(contenders))
+
+    # Match Starting Digit to Contenders
+    results = list(filter(lambda x: (x[i] == str(min_num)), contenders))
+    # print("Results:" + str(results))
+    contenders = results
+    i += 1
+    if len(contenders) == 1:
+        break
+
+co2_bi = ("".join([str(i) for i in results]))
+co2_int = int(co2_bi, 2)
 print("CO2_Binary:" + str(co2_bi))
 print("CO2_Int:" + str(co2_int))
-print(life_support)
+
+life_support = oxygen_int * co2_int
+print("Life_Support:" + str(life_support))
